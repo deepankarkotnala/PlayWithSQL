d2a670e09a1cf2cad6bcea1fa4ad75b2066983c1
Deepankar Kotnala <dkotnala5@gmail.com>
2020-02-16 13:08:15 +0530

    Update worker.sql

--- ORG_DB/worker.sql
+++ ORG_DB/worker.sql
@@ -71,12 +71,60 @@ INSERT INTO Title
  -- print first 3 characters from first_name from worker table
  select substring(first_name, 1,3) from worker;
  
- -- ------------------------------------------------------------------------------------
+-- ------------------------------------------------------------------------------------
  -- Find The Position Of The Alphabet (‘A’) In The First Name Column ‘Amitabh’ From Worker Table
  select instr(first_name, 'a') as Position_of_A from worker where first_name='Amitabh';
  -- INSTR function is case-insensitive by default
  -- BINARY function makes it case-sensitive
  
- -- ------------------------------------------------------------------------------------
-
+-- ------------------------------------------------------------------------------------
+-- Print The FIRST_NAME From Worker Table After Removing White Spaces From The Right Side.
+ select rtrim(first_name) from worker;
  
+-- ------------------------------------------------------------------------------------
+-- Print The DEPARTMENT From Worker Table After Removing White Spaces From The Left Side
+ select ltrim(department) from worker;
+ 
+-- ------------------------------------------------------------------------------------
+-- SQL Query To Print The FIRST_NAME From Worker Table After Replacing ‘a’ With ‘A’.
+select replace(first_name, 'a', 'A') as first_name from worker;
+
+-- ------------------------------------------------------------------------------------
+ -- SQL Query To Print The FIRST_NAME And LAST_NAME From Worker Table Into A Single Column COMPLETE_NAME. A Space Char Should Separate Them.
+ select concat(first_name,' ', last_name) Full_Name from worker;
+ 
+-- ------------------------------------------------------------------------------------
+-- SQL Query To Print All Worker Details From The Worker Table Order By FIRST_NAME Ascending
+  select * from worker order by first_name asc;
+
+-- ------------------------------------------------------------------------------------
+-- SQL Query To Print Details Of Workers Excluding First Names, “Vipul” And “Satish” From Worker Table
+select * from worker where first_name not in ('Vipul', 'Satish');
+
+-- ------------------------------------------------------------------------------------
+-- SQL Query To Print Details Of The Workers Who Have Joined In Feb’2014
+select * from worker where year(joining_date)=2014 and month(joining_date)=2;
+
+-- ------------------------------------------------------------------------------------
+-- SQL Query To Fetch Worker Names With Salaries >= 50000 And <= 100000.
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) As Worker_Name, Salary
+FROM worker 
+WHERE Salary BETWEEN 50000 AND 100000;
+
+-- ------------------------------------------------------------------------------------
+-- SQL Query To Fetch The Employee name having highest salary in the department order by the number of workers in each department in descending order.
+select concat(first_name,' ',last_name) Employee_name, Department, max(salary) as Highest_Salary, count(*) No_of_worker 
+from worker group by  Department 
+order by No_of_worker desc;
+
+-- ------------------------------------------------------------------------------------
+-- SQL Query To Print Details Of The Workers Who Are Also Managers.
+select concat(w.first_name,' ',last_name) Manager_Name, w.Worker_id, t.Affected_from, w.joining_date, w.department 
+from title t, worker w
+where t.worker_ref_id = w.worker_id
+and t.worker_title ='Manager';
+
+-- SQL Query To Fetch Duplicate Records Having Matching Data In Some Fields Of A Table.
+select * from title
+group by worker_title, affected_from
+having count(*) > 1;
